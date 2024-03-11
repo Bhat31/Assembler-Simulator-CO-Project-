eeb33a18beaac67a8370efb76ecb54525a7f60b8
Daksh345 <147997911+Daksh345@users.noreply.github.com>
2024-03-11 11:56:34 +0530

    Update Assembler.py

--- Assembler/Assembler.py
+++ Assembler/Assembler.py
@@ -1,37 +1,37 @@
 import re
 registers = {
-    "r0": "00000", #hardwired 0
-    "r1": "00001", #ra Return address
-    "r2": "00010", #sp Stack Pointer
-    "r3": "00011", #gp Global Pointer
-    "r4": "00100", #tp Thread Pointer
-    "r5": "00101", #t0 Temporary/alternate link register
-    "r6": "00110", #t1-2 Temporaries
-    "r7": "00111", #t1-2 Temporaries
-    "r8": "01000", #s0/fp Saved register/frame pointer
-    "r9": "01001", #s1 Saved Register
-    "r10": "01010",#a0-1 Function arguments/ return values
-    "r11": "01011",# a0-1 Function arguments/ return values
-    "r12": "01100",#Function arguments
-    "r13": "01101",#Function arguments
-    "r14": "01110",#Function arguments
-    "r15": "01111",#Function arguments
-    "r16": "10000",#Function arguments
-    "r17": "10001",#Function arguments
-    "r18": "10010", #s2 Saved registers
-    "r19": "10011", #s3 save registers;
-    "r20": "10100", #s4 saved register;
-    "r21": "10101", #s5 saved register
-    "r22": "10110", #s6 saved register
-    "r23": "10111", #s7 saved register
-    "r24": "11000", #s8 saved register
-    "r25": "11001", # s9 saved register
-    "r26": "11010", # s10 saved register
-    "r27": "11011", # s11 saved register
-    "r28": "11100", # t3 Temporaries
-    "r29": "11101", # t4 Temporaries
-    "r30": "11110", # t5 Temporaries
-    "r31": "11111", # t6 Temporaries
+    "zero": "00000", #hardwired 0
+    "ra": "00001", #ra Return address
+    "sp": "00010", #sp Stack Pointer
+    "gp": "00011", #gp Global Pointer
+    "tp": "00100", #tp Thread Pointer
+    "t0": "00101", #t0 Temporary/alternate link register
+    "t1": "00110", #t1-2 Temporaries
+    "t2": "00111", #t1-2 Temporaries
+    "s0": "01000", #s0/fp Saved register/frame pointer
+    "s1": "01001", #s1 Saved Register
+    "a0": "01010",#a0-1 Function arguments/ return values
+    "a1": "01011",# a0-1 Function arguments/ return values
+    "a2": "01100",#Function arguments
+    "a3": "01101",#Function arguments
+    "a4": "01110",#Function arguments
+    "a5": "01111",#Function arguments
+    "a6": "10000",#Function arguments
+    "a7": "10001",#Function arguments
+    "s2": "10010", #s2 Saved registers
+    "s3": "10011", #s3 save registers;
+    "s4": "10100", #s4 saved register;
+    "s5": "10101", #s5 saved register
+    "s6": "10110", #s6 saved register
+    "s7": "10111", #s7 saved register
+    "s8": "11000", #s8 saved register
+    "s9": "11001", # s9 saved register
+    "s10": "11010", # s10 saved register
+    "s11": "11011", # s11 saved register
+    "t3": "11100", # t3 Temporaries
+    "t4": "11101", # t4 Temporaries
+    "t5": "11110", # t5 Temporaries
+    "t6": "11111", # t6 Temporaries
 }
 oppcodes = { 'add': '0110011', 'sub': '0110011', 'sll': '0110011', 'slt': '0110011',
     'sltu': '0110011', 'xor': '0110011', 'srl': '0110011', 'or': '0110011',
@@ -41,6 +41,7 @@ oppcodes = { 'add': '0110011', 'sub': '0110011', 'sll': '0110011', 'slt': '01100
     'auipc': '0010111', 'lui': '0110111', 'jal': '1101111'}
 oppcodesforR= {"add":"000", "sub":"000", "sll":"001","slt":"010",
               "sltu":"011","xor":"100","srl":"101","or":"110","and":"111"}
+oppcodesforI={"lw":"010","addi":"000","sltiu":"011","jalr":"000"}
 def convert_to_binary(number):
     decimal_number=int(number)
     binary_representation=bin(decimal_number)
@@ -63,7 +64,7 @@ def assemble_instruction(instruction):
              final1.append(i)
     print(final1)
     return final1
-c = assemble_instruction("add r23, r1, r2")
+#c = assemble_instruction("add r23, r1, r2")
 def identificationforR(tokens):
     function7="0000000"
     if(tokens[0]=="sub"):
@@ -77,4 +78,20 @@ def identificationforR(tokens):
     final_output=final_output+function7
     print(final_output)
     print(len(final_output))
-identificationforR(c)
+#identificationforR(c)
+def identificationfori(tokens):
+    final_output=""
+    if(tokens[0]=="lw"):
+        binary_number=convert_to_binary[tokens[2]]
+        final_output=final_output+oppcodes[tokens[0]]
+        final_output=final_output+registers[tokens[1]]
+        final_output=final_output+oppcodesforI[tokens[0]]
+        final_output=final_output+registers[tokens[3]]
+        final_output=final_output+binary_number
+    else:
+        binary_number=convert_to_binary[tokens[2]]
+        final_output=final_output+oppcodes[tokens[0]]
+        final_output=final_output+registers[tokens[1]]
+        final_output=final_output+oppcodesforI[tokens[0]]
+        final_output=final_output+registers[tokens[3]]
+        final_output=final_output+binary_number
